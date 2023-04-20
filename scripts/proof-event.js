4c5880b21ccb794f9e62f9b81d180731ec6b0e92
lorenzoarlo <arlotti.lorenzo2@gmail.com>
2023-04-20 21:30:10 +0200

    refactoring

--- scripts/proof-event.js
+++
@@ -1,7 +0,0 @@
-function set_proof_state(event) 
-{
-    const sender = event.srcElement;
-    const proof_env = sender.parentElement.parentElement;
-    proof_env.querySelector(".proof-body").classList.toggle("hyde");
-    sender.innerText = (sender.innerText === 'visibility') ? 'visibility_off' : 'visibility';
-}
--- scripts/script.js
+++ scripts/script.js
@@ -1,90 +1,21 @@
-const searchbar = document.querySelector("#searchbar");
-const index_container = document.querySelector(".index-container");
+let footer = null;
+window.addEventListener("DOMContentLoaded", function() {
+    footer = document.querySelector("footer");
+    const scroll_to_bottom_btn = document.querySelector(".scroll-to-bottom-button");
+    const observer = new IntersectionObserver(function(e) {
+        scroll_to_bottom_btn.style.display = (e[0].isIntersecting) ? "none" : "grid"; 
+    });
+    observer.observe(footer);
 
-const main_index_leaf = index_container.querySelector("ul");    
-const index_tree = get_index_tree(main_index_leaf);
+})  
 
-function index_searchaction(event) 
-{
-    const value = event.srcElement.value.toLowerCase();
-    visit_index_all(index_tree, value);      
+function scroll_to_bottom() {
+    if(footer !== null) footer.scrollIntoView({"behavior": "smooth"});
 }
 
-
-function get_index_tree(node) 
-{
-    const tmp = {"object": node, "a-object": node.querySelector("a"), "content": "", "children": []};
-    tmp['content'] = tmp['a-object'].innerText.replaceAll("\n", "");
-    const children = Array.from(node.children);
-
-
-    for(let c of children) 
-    {
-        if('UL' === c.tagName) 
-        {
-            for(let b of c.children) 
-            {
-                tmp['children'].push(get_index_tree(b));
-            }
-        }
-        else if ('LI' === c.tagName)
-        {
-            tmp["children"].push(get_index_tree(c))
-        }
-    }
-    return tmp;
+function toggle_proof(event) {
+    const sender = event.srcElement;
+    const proof_env = sender.parentElement.parentElement;
+    proof_env.querySelector(".environment-body").classList.toggle("hyde");
+    sender.innerText = (sender.innerText === 'visibility') ? 'visibility_off' : 'visibility';
 }
-
-function visit_index_all(node, value) 
-{
-    let saturate = node['content'].toLowerCase().indexOf(value) !== -1;
-    let toShow = saturate;
-    
-    for(let c of node['children']) 
-    {
-        let child_content = visit_index_all(c, value);
-
-        toShow = toShow || child_content || saturate;
-    }
-
-
-    if(toShow) 
-    {
-        node['object'].classList.remove("hyde");
-        if(saturate) 
-        {
-            node['a-object'].classList.remove("desaturate");
-            node['object'].classList.remove("desaturate");
-        }
-        else 
-        {
-            node['a-object'].classList.add("desaturate");
-            node['object'].classList.add("desaturate");
-        }
-    }
-    else 
-    {
-        node['object'].classList.add("hyde");
-    }
-
-
-
-    return toShow;
-    // if(desaturate)
-    //     
-    // else
-    //     node['object'].classList.remove("desaturate");
-    
-    
-    
-    // for(let c of node['children']) 
-    // {
-    //     child_content = visit_all(c, value);
-    //     toShow = toShow || child_content || (!desaturate);
-    // }
-    
-    // if(toShow) 
-    // else 
-    
-} 
-
